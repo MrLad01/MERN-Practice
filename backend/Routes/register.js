424efdd8a24dfa8c6fe4d238b4583bd618184699
MrLad01 <ladayodeji@gmail.com>
2023-07-09 20:41:09 +0100

    update

--- backend/Routes/register.js
+++ backend/Routes/register.js
@@ -8,10 +8,10 @@ router.post('/register', async(req, res) => {
     const UserFile = await User.findOne({$or: [{id}, {email}, {ip_address}]})
 
     if(UserFile){
-        res.sendStatus(400).send({msg: "You motherfucker again!!!"})
+        res.sendStatus(400).json({msg: "You motherfucker again!!!"})
     } else {
         const newUser = await User.create({id, first_name, last_name, email, gender, ip_address, password})
-        res.sendStatus(200).send({msg:"Welcome to our association, you bloody thwart"})
+        res.sendStatus(200).json({msg:"Welcome to our association, you bloody thwart"})
     }
 })
 
