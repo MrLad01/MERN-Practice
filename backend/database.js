6dd45235f45e0f9890d0d0d7214b81c2eccfbff7
MrLad01 <ladayodeji@gmail.com>
2023-07-09 04:14:31 +0100

    update

---
+++ backend/database.js
@@ -0,0 +1,7 @@
+const { default: mongoose } = require('mongoose');
+
+require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/practice')
+    .then(() => console.log('Connected to DB'))
+    .catch((err) => console.log(err));
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
+const cookieParser = require('cookie-parser');
 const express = require('express');
 const app = express();
 
+require('./database')
 
 app.listen(4000, ()=>{
     console.log("Hello");
 })
 
 app.use(express.json());
+app.use(cookieParser());
+
 // app.use(express.urlencoded());
 app.use((req, res) => {
     console.log(`${req.url} : ${req.method}`)
