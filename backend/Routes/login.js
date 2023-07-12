39ddbde142a4b1607176e492bab358e9dccaedb9
MrLad01 <ladayodeji@gmail.com>
2023-07-12 22:11:48 +0100

    update

--- backend/Routes/login.js
+++ backend/Routes/login.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const router = Router();
-const User = require('../Models/userSchema')
+const User = require('../Models/userSchema');
+const { comparePassword } = require('../Utils/helpers');
 
 router.post('/login', async(req, res) => {
     const { email, password } = req.body
@@ -8,8 +9,9 @@ router.post('/login', async(req, res) => {
     if(!email || !password ) res.send(400)
     const UserFile = await User.findOne({email})
     if(!UserFile) return res.sendStatus(401);
+    const isValid = comparePassword(password, UserFile.password)
     
-    if(password === UserFile.password) {
+    if(isValid) {
         req.session.user = UserFile
         return res.sendStatus(200)
     } else {
--- backend/Routes/register.js
+++ backend/Routes/register.js
@@ -1,17 +1,18 @@
 const  { Router } = require('express');
 const router = Router();
 const User = require('../Models/userSchema')
-require('../Utils/helpers')
+const { hashPassword } = require('../Utils/helpers')
 
 
 router.post('/register', async(req, res) => {
     const {id, first_name, last_name, email, gender, ip_address, password} = req.body
     const UserFile = await User.find({$or: [{id}, {email}, {ip_address}]})
+    const hashed = hashPassword(password)
 
     if(UserFile){
         res.status(400).send({msg: "You motherfucker again!!!"})
     } else {
-        const newUser = await User.create({id, first_name, last_name, email, gender, ip_address, password})
+        const newUser = await User.create({id, first_name, last_name, email, gender, ip_address, password: hashed})
         res.status(200).send({msg:"Welcome to our association, you bloody thwart"})
     }
 })
