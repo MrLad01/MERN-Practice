14e9cf8a8fd8a3d2c3e9c4f2e8250db86056ef56
MrLad01 <ladayodeji@gmail.com>
2023-07-14 22:02:20 +0100

    update

--- backend/Routes/register.js
+++ backend/Routes/register.js
@@ -6,23 +6,19 @@ const passport = require('passport');
 require('../Strategies/local')
 
 
-// router.post('/register', async(req, res) => {
-//     const {id, first_name, last_name, email, gender, ip_address, password} = req.body
-//     const UserFile = await User.find({$or: [{id}, {email}, {ip_address}]})
-//     const hashed = hashPassword(password)
+router.post('/register', async(req, res) => {
+    const {id, first_name, last_name, email, gender, ip_address, password} = req.body
+    const UserFile = await User.find({$or: [{id}, {email}, {ip_address}]})
+    const hashed = hashPassword(password)
 
-//     if(UserFile){
-//         res.status(400).send({msg: "You motherfucker again!!!"})
-//     } else {
-//         const newUser = await User.create({id, first_name, last_name, email, gender, ip_address, password: hashed})
-//         res.status(200).send({msg:"Welcome to our association, you bloody thwart"})
-//     }
-// })
+    if(UserFile){
+        res.status(400).send({msg: "You motherfucker again!!!"})
+    } else {
+        const newUser = await User.create({id, first_name, last_name, email, gender, ip_address, password: hashed})
+        res.status(200).send({msg:"Welcome to our association, you bloody thwart"})
+    }
+})
 
-router.post('/register', passport.authenticate('local', (req, res)=> {
-    console.log('Logged in!');
-    res.status(200).send('Welcome to the association you bloody thwart')
-}))
 
 
 
--- backend/Strategies/local.js
+++ backend/Strategies/local.js
@@ -8,20 +8,22 @@ const { comparePassword } = require('../Utils/helpers');
 passport.use(
     new Strategy({
         usernameField: "email"
-    }, async(id, first_name, last_name,email,gender, ip_address, password, done) => {
+    }, async(email, password, done) => {
         console.log(email)
         console.log(password)
         try{
             if(!email || !password){
-                done(Error("Please fill in your details", null, null))
+                done("Bad Request, missing credentials", null, null)
             }
-            const userFile = await User.find({$or: [{id}, {email}, {ip_address}]})
-            const hashed = hashPassword(password)
-            if(userFile){
-                done(Error("You motherfuckr again!!!"), null, null)
+            const userFile = await User.findOne({email});
+            if(!userFile) throw Error('User not Found');
+            const isValid = comparePassword(password, userFile.password);
+            if(isValid){
+                console.log("Authenticated Successfully");
+                done(null, userFile)
             } else {
-                const newUser = User.create({id, first_name, last_name,email,gender, ip_address, password})
-                done(null, newUser, null)
+                console.log("Failed to Authenticate");
+                done(null, null)
             }
         } catch{
             console.error(err);
@@ -29,30 +31,3 @@ passport.use(
         }
     })
 )
-
-// passport.use(
-//     new Strategy({
-//         usernameField: "email"
-//     }, async(email, password, done) => {
-//         console.log(email)
-//         console.log(password)
-//         try{
-//             if(!email || !password){
-//                 done("Bad Request, missing credentials", null, null)
-//             }
-//             const userFile = await User.findOne({email});
-//             if(!userFile) throw Error('User not Found');
-//             const isValid = comparePassword(password, userFile.password);
-//             if(isValid){
-//                 console.log("Authenticated Successfully");
-//                 done(null, userFile)
-//             } else {
-//                 console.log("Failed to Authenticate");
-//                 done(null, null)
-//             }
-//         } catch{
-//             console.error(err);
-//             done(err, null)
-//         }
-//     })
-// )
