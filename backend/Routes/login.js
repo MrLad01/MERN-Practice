b208bee1c253436fad1e572d9041b5b48fc0d8fb
MrLad01 <ladayodeji@gmail.com>
2023-07-14 22:26:41 +0100

    update

--- backend/Routes/login.js
+++ backend/Routes/login.js
@@ -6,22 +6,6 @@ const passport = require('passport')
 require('../Strategies/local')
 
 
-// router.post('/login', async(req, res) => {
-//     const { email, password } = req.body
-
-//     if(!email || !password ) res.send(400)
-//     const UserFile = await User.findOne({email})
-//     if(!UserFile) return res.sendStatus(401);
-//     const isValid = comparePassword(password, UserFile.password)
-    
-//     if(isValid) {
-//         req.session.user = UserFile
-//         return res.sendStatus(200)
-//     } else {
-//         return res.sendStatus(401)
-//     }
-// })
-
 router.post('/login', passport.authenticate('local'), (req, res)=> {
     console.log('Logged in!');
     res.status(200).send('Welcome to the association you bloody thwart')
