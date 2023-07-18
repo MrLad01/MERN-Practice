777ea6a7b4fc3a0a759c7a81602069520f567062
MrLad01 <ladayodeji@gmail.com>
2023-07-18 22:20:51 +0100

    update

--- frontend1/src/pages/Login.jsx
+++ frontend1/src/pages/Login.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-      
-    </div>
+    <React.Fragment>
+        <Form>
+            <label htmlFor="email">E-mail</label>
+            <input type="text" placeholder='Enter your email address' />
+            <label htmlFor="password">Password</label>
+            <input type="text" placeholder='Enter your password' />
+        </Form>
+    </React.Fragment>
   )
 }
 
