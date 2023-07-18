857ad69b2d91c9233cde1a5edae3cbb81b4e7ed7
MrLad01 <ladayodeji@gmail.com>
2023-07-18 22:33:47 +0100

    update

--- frontend1/src/pages/Login.jsx
+++ frontend1/src/pages/Login.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form } from 'react-router-dom'
 
 const Login = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+
   return (
     <React.Fragment>
         <Form className='login'>
             <label htmlFor="email">E-mail</label>
-            <input type="text" placeholder='Enter your email address' />
+            <input type="email" placeholder='Enter your email address' value={email} onChange={(e)=> setEmail(e.target.value)} />
             <label htmlFor="password">Password</label>
-            <input type="text" placeholder='Enter your password' />
+            <input type="password" placeholder='Enter your password' value={password} onChange={(e)=> setPassword(e.target.value)} />
             <button type="submit">Log in</button>
         </Form>
     </React.Fragment>
