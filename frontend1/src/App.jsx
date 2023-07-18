e3cb51ab5e2a0e7bc365ce280be3623cf8b65511
MrLad01 <ladayodeji@gmail.com>
2023-07-18 22:16:04 +0100

    update

--- frontend1/src/App.jsx
+++ frontend1/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { createBrowserRouter, Route, RouterProvider, createRoutesFromElements } from 'react-router-dom'
+import Login from './pages/Login'
 
 const App = () => {
+
+  const route = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path='/login' element={<Login />} />
+      </Route>
+    )
+  )
+
   return (
-    <>
-      
-    </>
+    <RouterProvider route={route} />
   )
 }
 
