1b6e8dc8b903c446dc31b0d9011f5e6c9e250b28
MrLad01 <ladayodeji@gmail.com>
2023-07-19 22:01:22 +0100

    update

--- frontend1/src/pages/Register.jsx
+++ frontend1/src/pages/Register.jsx
@@ -8,6 +8,7 @@ const Register = () => {
     const [gender, setGender] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [password1, setPassword1] = useState('')
     id, ip_address
 
 
@@ -18,6 +19,9 @@ const Register = () => {
             <input type="email" name='email' placeholder='Enter your email address' value={email} onChange={(e)=> setEmail(e.target.value)} />
             <label htmlFor="password">Password</label>
             <input type="password" name='password' placeholder='Enter your password' value={password} onChange={(e)=> setPassword(e.target.value)} />
+            <label htmlFor="password1">Confirm Password</label>
+            <input type="password" value={password1} onChange={(e)=> setPassword1(e.target.value)} />
+            {password === password1 ? <></> : <p>Password does not match</p>}
             <button type="submit">Log in</button>
         </Form>
         <p>If you already have an account<Link to="/login"> click here to login</Link></p>
