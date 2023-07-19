5ef54050e58bbb2d0bd126ce1e4dd100e43e4a16
MrLad01 <ladayodeji@gmail.com>
2023-07-19 22:56:59 +0100

    update

--- frontend1/src/pages/Register.jsx
+++ frontend1/src/pages/Register.jsx
@@ -3,18 +3,37 @@ import { Form, Link } from 'react-router-dom'
 
 const Register = () => {
 
+
+    const [id, setId] = useState('')
     const [first_name, setFirst_name] = useState('')
     const [last_name, setLast_name] = useState('')
     const [gender, setGender] = useState('')
     const [email, setEmail] = useState('')
+    const [ip_address, setIp_address] = useState('')
     const [password, setPassword] = useState('')
     const [password1, setPassword1] = useState('')
     // id, ip_address
 
+    async function submit(e) {
+      e.preventDefault();
+      try{
+        const body = { id, first_name,last_name, gender, ip_address, email, password }
+        const response = await fetch('http://localhost:4000/user/register', {
+          method: 'POST',
+          headers: {"Content-Type": "application/json"},
+          body: JSON.stringify(body)
+        });
+
+        console.log(response);
+      } catch(err){
+        console.error(err.message)
+      }
+    }
+
 
   return (
     <React.Fragment>
-        <Form method='post' className='register'>
+        <Form method='post' className='register' onSubmit={submit}>
             <label htmlFor="email">E-mail</label>
             <input type="email" name='email' placeholder='Enter your email address' value={email} onChange={(e)=> setEmail(e.target.value)} />
             <label htmlFor="password">Password</label>
