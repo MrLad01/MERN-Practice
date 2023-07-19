ea896d28f90c896af3ff8a6f6d46df8672ca2514
MrLad01 <ladayodeji@gmail.com>
2023-07-19 22:51:30 +0100

    update

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ const passport = require('passport')
 const cors = require('cors')
 
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
@@ -22,7 +23,6 @@ app.use(session({
         mongoUrl: "mongodb://127.0.0.1:27017/practice"
     })
 }))
-app.use(cors);
 
 app.listen(4000, ()=>{
     console.log("Hello");
