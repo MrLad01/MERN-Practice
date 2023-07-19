24c45d847b8ccfab1cf646c9cb543d7ba4a1d459
MrLad01 <ladayodeji@gmail.com>
2023-07-19 21:55:47 +0100

    update

--- frontend1/src/pages/Register.jsx
+++ frontend1/src/pages/Register.jsx
@@ -13,11 +13,11 @@ const Register = () => {
 
   return (
     <React.Fragment>
-        <Form className='register'>
+        <Form method='post' className='register'>
             <label htmlFor="email">E-mail</label>
-            <input type="email" placeholder='Enter your email address' value={email} onChange={(e)=> setEmail(e.target.value)} />
+            <input type="email" name='email' placeholder='Enter your email address' value={email} onChange={(e)=> setEmail(e.target.value)} />
             <label htmlFor="password">Password</label>
-            <input type="password" placeholder='Enter your password' value={password} onChange={(e)=> setPassword(e.target.value)} />
+            <input type="password" name='password' placeholder='Enter your password' value={password} onChange={(e)=> setPassword(e.target.value)} />
             <button type="submit">Log in</button>
         </Form>
         <p>If you already have an account<Link to="/login"> click here to login</Link></p>
