7450703748678fa5b1c7acc94ace1297c5bc7bc5
MrLad01 <ladayodeji@gmail.com>
2023-07-28 14:26:48 +0100

    update

--- frontend1/src/pages/Home.jsx
+++ frontend1/src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useAuthContext } from '../../context/AuthContextProvider';
 
 const Home = () => {
-  const { last_name } = useParams();
+  const { last_name } = useAuthContext();
   const [userData, setUserData] = useState(null);
 
   useEffect(() => {
@@ -28,14 +29,14 @@ const Home = () => {
     <div>
       {userData ? (
         <>
-          <h1>Welcome Back, {userData[0].gender == "Female" ? "Mrs." : "Mr."}{userData[0].first_name}</h1>
-          <h3>Please confirm your data {userData[0].gender == "Male" ?"sir":"Ma"}</h3>
-          <h5>id: {userData[0].id}</h5>
-          <h5>first_name: {userData[0].first_name}</h5>
-          <h5>last_name: {userData[0].last_name}</h5>
-          <h5>email: {userData[0].email}</h5>
-          <h5>gender: {userData[0].gender}</h5>
-          <h5>ip_address: {userData[0].ip_address}</h5>
+          <h1>Welcome Back, {userData.gender == "Female" ? "Mrs." : "Mr."}{userData.first_name}</h1>
+          <h3>Please confirm your data {userData.gender == "Male" ?"sir":"Ma"}</h3>
+          <h5>id: {userData.id}</h5>
+          <h5>first_name: {userData.first_name}</h5>
+          <h5>last_name: {userData.last_name}</h5>
+          <h5>email: {userData.email}</h5>
+          <h5>gender: {userData.gender}</h5>
+          <h5>ip_address: {userData.ip_address}</h5>
           {/* Render other user data here */}
         </>
       ) : (
